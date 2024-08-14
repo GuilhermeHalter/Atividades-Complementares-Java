54b50d4e7daa6bad1535b1106271911bf975812c
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-14 10:13:57 -0300

    Done Exercise 1

--- src/Questao1/Main.java
+++ src/Questao1/Main.java
@@ -1,4 +1,20 @@
 package Questao1;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor de A: ");
+        int a = scanner.nextInt();
+        System.out.println("Digite o valor de B: ");
+        int b = scanner.nextInt();
+
+        Valores valores = new Valores(a, b);
+
+        for(int x = valores.A; x <= valores.B; x++) {
+            System.out.println(x);
+        }
+
+    }
 }
--- src/Questao1/Valores.java
+++ src/Questao1/Valores.java
@@ -1,4 +1,11 @@
 package Questao1;
 
 public class Valores {
+    int A;
+    int B;
+
+    public Valores(int A, int B) {
+        this.A = A;
+        this.B = B;
+    }
 }
